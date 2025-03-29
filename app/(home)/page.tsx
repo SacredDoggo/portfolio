bc730df6dab68eb7b615f70a0e9d6a3ab90aaf53
Om Vipul Krishna <omvkrishna0852@gmail.com>
2025-03-29 11:16:24 +0530

    dynamic theme

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,11 @@
 const HomePage = () => {
   return (
-    <div className="h-full w-full">
-      
+    <div className="h-full flex flex-col gap-y-10 p-4">
+      <span>Span</span>
+      <p>P Tag</p>
+      <button>Hello</button>
+      <h1>H1 Tag</h1>
+      <h2>H2 Tag</h2>
     </div>
   );
 };
